75ab01b450333e289b0e50af32b5c343d5224d43
Pius <bimalover9@gmail.com>
2025-07-18 18:36:06 +0300

    contact section

--- javascript.js
+++ javascript.js
@@ -26,13 +26,43 @@ window.addEventListener('scroll', () => {
 
     lastScrollY = currentScrollY;
 });
-  const btn1 = document.getElementById("btn1");
+//   const btn1 = document.getElementById("btn1");
+//   const btn2 = document.getElementById("btn2");
+
+//   function selectButton(selected, unselected) {
+//     selected.classList.add("selected");
+//     unselected.classList.remove("selected");
+//   }
+
+//   btn1.addEventListener("click", () => selectButton(btn1, btn2));
+// btn2.addEventListener("click", () => selectButton(btn2, btn1));
+  
+const btn1 = document.getElementById("btn1");
   const btn2 = document.getElementById("btn2");
+  const education = document.getElementById("Education");
+  const experience = document.getElementById("Experience");
 
-  function selectButton(selected, unselected) {
-    selected.classList.add("selected");
-    unselected.classList.remove("selected");
-  }
+  btn1.addEventListener("click", () => {
+    btn1.classList.add("selected");
+    btn2.classList.remove("selected");
+    education.classList.add("active");
+    experience.classList.remove("active");
+  });
 
-  btn1.addEventListener("click", () => selectButton(btn1, btn2));
-  btn2.addEventListener("click", () => selectButton(btn2, btn1));
+  btn2.addEventListener("click", () => {
+    btn2.classList.add("selected");
+    btn1.classList.remove("selected");
+    experience.classList.add("active");
+    education.classList.remove("active");
+  });
+  const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        } else {
+          entry.target.classList.remove("show");
+        }
+      });
+    });
+
+    document.querySelectorAll('.descriptions').forEach(el => observer.observe(el));
