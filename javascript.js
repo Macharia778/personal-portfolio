592fff45206f1d45d2b2f379b57d3b51c3def284
Pius <bimalover9@gmail.com>
2025-08-01 19:16:29 +0300

    friday

--- javascript.js
+++ javascript.js
@@ -29,33 +29,7 @@ window.addEventListener('scroll', () => {
 });
 
   
-const btn1 = document.getElementById("btn1");
-  const btn2 = document.getElementById("btn2");
-  const education = document.getElementById("Education");
-  const experience = document.getElementById("Experience");
 
-  btn1.addEventListener("click", () => {
-    btn1.classList.add("selected");
-    btn2.classList.remove("selected");
-    education.classList.add("active");
-    experience.classList.remove("active");
-  });
-
-  btn2.addEventListener("click", () => {
-    btn2.classList.add("selected");
-    btn1.classList.remove("selected");
-    experience.classList.add("active");
-    education.classList.remove("active");
-  });
-  const observer = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("show");
-        } else {
-          entry.target.classList.remove("show");
-        }
-      });
-    });
 
 document.querySelectorAll('.descriptions').forEach(el => observer.observe(el));
     (function() {
